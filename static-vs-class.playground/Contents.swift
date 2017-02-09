6e949d67220c8f52b33af4d9cc68afeb510237d6
Horst LEUNG <horst.leung@iheha.com>
2017-02-09 17:30:11 +0800

    Static vs class

---
+++ static-vs-class.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class A {
+    class func classFunction() {
+    }
+    static func staticFunction() {
+    }
+    class func classFunctionToBeMakeFinalInImmediateSubclass() {
+    }
+}
+
+class B: A {
+    override class func classFunction(){
+        
+    }
+    
+    //Compile Error. Class method overrides a 'final' class method
+    override static func staticFunction() {
+        
+    }
+    
+    override final class func classFunctionToBeMakeFinalInImmediateSubclass() {
+    }
+
+}
+
+class C: B{
+    override class func classFunction() {
+    }
+    
+    //Compile Error. Class method overrides a 'final' class method
+    override static func classFunctionToBeMakeFinalInImmediateSubclass(){
+        
+    }
+}
+
+struct sa {
+    static func staticFunction() {}
+    class func classFunction() {}
+}
